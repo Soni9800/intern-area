f0e0d25e266e187c1c23ba90e4df711de8418cbc
Soni9800 <sahuaradhana50@gmail.com>
2026-09-18 21:11:22 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
-
-  output: "export",
-
-  basePath: "/intern-area",
-  assetPrefix: "/intern-area/",
-
-  turbopack: {
-    root: path.join(__dirname),
-  },
 };
 
 export default nextConfig;
